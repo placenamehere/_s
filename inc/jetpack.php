26a5f4cc15e40907f535e178d04f1daf04e3b4d1
Chris Casciano <chris@placenamehere.com>
2016-03-03 10:25:38 -0500

    namespace changes per readme

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -4,7 +4,7 @@
  *
  * @link https://jetpack.me/
  *
- * @package _s
+ * @packagePnhtwentysixteen
  */
 
 /**
@@ -13,23 +13,23 @@
  * See: https://jetpack.me/support/infinite-scroll/
  * See: https://jetpack.me/support/responsive-videos/
  */
-function _s_jetpack_setup() {
+function pnhtwentysixteen_jetpack_setup() {
 	// Add theme support for Infinite Scroll.
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
-		'render'    => '_s_infinite_scroll_render',
+		'render'    => 'pnhtwentysixteen_infinite_scroll_render',
 		'footer'    => 'page',
 	) );
 
 	// Add theme support for Responsive Videos.
 	add_theme_support( 'jetpack-responsive-videos' );
 }
-add_action( 'after_setup_theme', '_s_jetpack_setup' );
+add_action( 'after_setup_theme', 'pnhtwentysixteen_jetpack_setup' );
 
 /**
  * Custom render function for Infinite Scroll.
  */
-function _s_infinite_scroll_render() {
+function pnhtwentysixteen_infinite_scroll_render() {
 	while ( have_posts() ) {
 		the_post();
 		if ( is_search() ) :
